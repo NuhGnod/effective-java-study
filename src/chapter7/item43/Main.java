a287facf2315f6ff290917bdb0f6a52334253e03
Kimdonghun <zhfptm12@naver.com>
2023-07-31 23:29:41 +0900

    한정적 비한정적 차이

--- src/chapter7/item43/Main.java
+++ src/chapter7/item43/Main.java
@@ -1,15 +1,36 @@
 package chapter7.item43;
 
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
+
         HOP hop = function1(HOP::new);// 정적. 0살
         boolean b = function2(HOP.now()::isOlder, hop);// 한정적
         System.out.println(b);
         boolean b1 = function3(HOP::isAdult, hop);// 비한정적
         System.out.println("b1 = " + b1);
+
+        List<String> words = Arrays.asList("ABC", "CD", "EFG");
+
+        // 인자로 넘어온 객체의 메소드를 호출
+        // 람다식으로 표현
+        words.stream().map(word -> word.toLowerCase())
+                .forEach(word -> System.out.println(word));
+
+        // 메소드 참조로 표현
+        words.stream().map(String::toLowerCase) // 비한정적.
+                .forEach(word -> System.out.println(word));
+        List<HOP> hops = Arrays.asList(new HOP(1), new HOP(10), new HOP(5));
+
+        // 새로운 HOP객체가 List의 hop들보다 오래되었냐??
+        hops.stream().map(HOP.now()::isOlder).forEach(old -> System.out.println(old));
+        hops.stream().map(HOP::isAdult).forEach(adult -> System.out.println(adult));
+
     }
 
 
